ba1fbc580cb1677a3f1799bc13ab110f66a24208
Ziga Bobnar <ziga@bobnar.net>
2019-12-06 23:58:58 +0100

    Templates, showing demo data

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,6 +11,16 @@ class IndexController extends AbstractController {
      * @Route("/")
      */
     public function index() {
-        return $this->render('index/index.html.twig');
+        $ads = [
+            ['title' => 'Ad 1', 'features' => 'mklfds. vfds.v ds.ff vds.f vd.s fv.sd'],
+            ['title' => 'Ad 2', 'features' => 'mklfds. vfds.v ds.ff vds.f vd.s fv.sd'],
+            ['title' => 'Ad 3', 'features' => 'mklfds. vfds.v ds.ff vds.f vd.s fv.sd'],
+            ['title' => 'Ad 4', 'features' => 'mklfds. vfds.v ds.ff vds.f vd.s fv.sd'],
+            ['title' => 'Ad 5', 'features' => 'mklfds. vfds.v ds.ff vds.f vd.s fv.sd']
+        ];
+
+        return $this->render('index/index.html.twig', [
+            'ads' => $ads
+        ]);
     }
 }
