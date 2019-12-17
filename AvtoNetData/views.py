3dd9444d23b866faedc4c1b4382cb60f7b69c4ee
Ziga Bobnar <ziga@bobnar.net>
2019-12-17 13:20:31 +0100

    Use render() shortcut

--- AvtoNetData/views.py
+++ AvtoNetData/views.py
@@ -1,16 +1,16 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 from .models import CarAd
 
 def index(request):
     latestAdsList = CarAd.objects.order_by('-first_seen_on')[:5]
-    template = loader.get_template('AvtoNetData/index.html')
     context = {
         'latestAdsList': latestAdsList,
     }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, 'AvtoNetData/index.html', context)
 
 def detail(request, id):
     return HttpResponse('Looking at ad %s' % id)
