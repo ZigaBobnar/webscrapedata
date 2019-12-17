0301eb7576852e797adbe187642de281a7c27250
Ziga Bobnar <ziga@bobnar.net>
2019-12-17 12:54:06 +0100

    Add basic avtonet models

--- AvtoNetData/models.py
+++ AvtoNetData/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class CarAd(models.Model):
+    avtonetId = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
+    price = models.CharField(max_length=20)
+    first_seen_on = models.DateTimeField('First seen date')
+
+class AdData(models.Model):
+    carAd = models.ForeignKey(CarAd,
+        on_delete=models.CASCADE)
+    key = models.CharField(max_length=200)
+    value = models.CharField(max_length=200)
+    updated_on = models.DateTimeField('Updated on date')
