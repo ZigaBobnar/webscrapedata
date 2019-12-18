fc537b1716f69228b5380df7e17ff8183a4c0767
Ziga Bobnar <ziga@bobnar.net>
2019-12-18 08:03:13 +0100

    Release constraints to make it easier to add new objects

--- avtonet/admin.py
+++ avtonet/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import CarAd, AdData
 
 class CarAdAdmin(admin.ModelAdmin):
-    fields = ['title']
+    fields = ['title', 'first_seen_on']
 
 admin.site.register(CarAd, CarAdAdmin)
