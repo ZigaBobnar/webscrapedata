b81f4b1326434a7caffd643672e4b2c5101ac5c2
Ziga <ziga@bobnar.net>
2019-12-18 13:56:43 +0200

    Add filter to admin page

--- avtonet/admin.py
+++ avtonet/admin.py
@@ -4,5 +4,7 @@ from .models import CarAd, AdData
 
 class CarAdAdmin(admin.ModelAdmin):
     fields = ['title', 'first_seen_on']
+    
+    list_filter = ['first_seen_on']
 
 admin.site.register(CarAd, CarAdAdmin)
