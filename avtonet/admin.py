5cb3e895b4cc3907186e2d838acb75f3bf72db0b
Ziga <ziga@bobnar.net>
2019-12-18 14:12:34 +0200

    Add some structure to the ad list

--- avtonet/admin.py
+++ avtonet/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import CarAd, AdData
 
 class CarAdAdmin(admin.ModelAdmin):
-    fields = ['title', 'first_seen_on']
+    fields = ['title', 'price', 'first_seen_on']
     
     list_filter = ['first_seen_on']
 
